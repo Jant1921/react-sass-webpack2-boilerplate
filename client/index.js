0b996385e0c8d7a262173fbf8b4ca12d43714472
Jose Ruiz <jruizj6@hotmail.com>
2017-02-23 10:18:54 -0600

    sass support added

--- client/index.js
+++ client/index.js
@@ -5,5 +5,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+require('./sass/main.scss');
 
 ReactDOM.render(<App />,document.getElementById('root'));
